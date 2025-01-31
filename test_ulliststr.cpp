a4bcbdad03e0093a8c9a20d335ec722710d78643
Kelly Lukito <klukito@usc.edu>
2025-01-31 19:09:34 +0000

    hw1 pt2 done

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,104 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
 
+//helper function to print list
+void printList(ULListStr&list){
+  cout << "List (size=" << list.size() << "): ";
+  for(size_t i=0; i<list.size(); i++){
+    cout << list.get(i) << " ";
+  }
+  cout << endl;
+}
+
 int main(int argc, char* argv[])
 {
+  ULListStr dat;
+  //test 1: empty list
+  cout << "\n=== Test 1: Empty List ===\n";
+  cout << "Empty list size: " << dat.size() << endl;
 
+  //test 2: push_back on empty list and multiple items
+  cout << "\n=== Test 2: Push_back test ===\n";
+  dat.push_back("a");
+  cout << "After first push_back: ";
+  printList(dat);
+  cout << "Front: " <<dat.front() << "Back: " << dat.back() << endl;
+
+  //add more items to test array fill
+  for(int i=0; i<10; i++){
+    dat.push_back(to_string(i));
+  }
+  cout << "After multiple push_backs: ";
+  printList(dat);
+
+  //test 3: push_front
+  cout << "\n=== Test 3: Push_front test ===\n";
+  dat.push_front("front1");
+  dat.push_front("front2");
+  cout << "After push_fronts: ";
+  printList(dat);
+  cout << "Front: " << dat.front() << ", Back: " << dat.back() << endl;
+
+  //test 4: pop operations
+  cout <<"\n=== Test 4: Pop tests ===\n";
+  dat.pop_front();
+  cout << "After pop_front: ";
+  printList(dat);
+
+  dat.pop_back();
+  cout << "After pop_back: ";
+  printList(dat);
+
+  //test 5: edge cases - empty list
+  cout << "\n=== Test 5: Empty list through pops ===\n";
+  while(dat.size() > 0){
+    cout << "Popping back: " << dat.back() << endl;
+    dat.pop_back();
+  }
+  cout << "Size after emptying: " << dat.size() << endl;
+
+  //test 6: multiple operations
+  cout << "\n=== Test 6: Mulitple operations sequence ===\n";
+  dat.push_back("first");
+  dat.push_back("second");
+  dat.push_front("front");
+  cout << "After sequence: ";
+  printList(dat);
+
+  //test 7: test array wraparound
+  cout << "\n=== Test 7: Array wraparound test ===\n";
+  ULListStr wrap;
+  //fill first node
+  for(int i=0; i<10; i++){
+    wrap.push_back(to_string(i));
+  }
+  //add to front to test wraparound
+  wrap.push_front("wrap1");
+  wrap.push_front("wrap2");
+  cout << "After wraparound: ";
+  for(size_t i=0; i<wrap.size(); i++){
+    cout << wrap.get(i) << " ";
+  }
+  cout << endl;
+
+  //test 8: get value at location
+  cout << "\n=== Test 8: Get value at location ===\n";
+  cout << "First value: " << wrap.get(0) << endl;
+  cout << "Middle value: " << wrap.get(wrap.size()/2) << endl;
+  cout << "Last value: " << wrap.get(wrap.size()-1) << endl;
+
+  try{
+    cout << "Attempting to access invalid location..." << endl;
+    wrap.get(100); //throw exception
+  }
+  catch(const std::invalid_argument& e){
+    cout << "Successfully caught invalid location access" << endl;
+  }
+
+  cout << "\nAll tests completed!\n";
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,134 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+//add an item to back of list
+void ULListStr::push_back(const std::string& val){
+  //if empty list, create first item
+  if(empty()){
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    head_ = tail_ = newItem;
+  }
+  //if tail item's array is full, create new item
+  else if(tail_->last == ARRSIZE){
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    newItem->prev = tail_;
+    tail_->next = newItem;
+    tail_ = newItem;
+  }
+  //Add to existing tail item's array
+  else{
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+  }
+  size_++;
+}
+
+//add item to front of list
+void ULListStr::push_front(const std::string& val){
+  //if empty list, create first item
+  if(empty()){
+    Item* newItem = new Item;
+    newItem->val[0] = val;
+    newItem->first = 0;
+    newItem->last = 1;
+    head_ = tail_ = newItem;
+  }
+
+  //if no space at front of head item's array, create new item
+  else if(head_->first == 0){
+    Item* newItem = new Item;
+    newItem->val[ARRSIZE-1] = val;
+    newItem->first = ARRSIZE-1;
+    newItem->last = ARRSIZE;
+    newItem->next = head_;
+    head_->prev = newItem;
+    head_ = newItem;
+  }
+  //add to front of existing head item's Array
+  else{
+    head_->first--;
+    head_->val[head_->first] = val;
+  }
+  size_++;
+}
+
+//remove last item in list
+void ULListStr::pop_back(){
+  if(empty()) return;
+
+  //decrease the last index and size
+  tail_->last--;
+  size_--;
+
+  //if tail item is empty, remove it
+  if(tail_->first == tail_->last){
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if(tail_==NULL){
+      head_ = NULL; //list now empty
+    }
+    else{
+    tail_->next = NULL;
+    }
+    delete temp;
+  }
+}
+
+//remove first item
+void ULListStr::pop_front(){
+  if(empty()) return;
+
+  //increase first index and decrease size
+  head_->first++;
+  size_--;
+
+  //if head becomes empty, remove it
+  if(head_->first == head_->last){
+    Item* temp = head_;
+    head_ = head_->next;
+    if(head_==NULL){
+      tail_ = NULL; //list now empty
+    }
+    else{
+    head_->prev = NULL;
+    }
+    delete temp;
+  }
+}
+
+//return last item
+std::string const& ULListStr::back() const{
+  return tail_->val[tail_->last-1];
+}
+
+//return first item
+std::string const& ULListStr::front() const{
+  return head_->val[head_->first];
+}
+
+//get pointer to value at given loc
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  //check valid location
+  if(loc>=size_) return NULL;
+  
+  Item*curr = head_;
+  size_t currLoc = 0;
+
+  //go through list
+  while(curr!=NULL){
+    if(currLoc + (curr->last - curr->first) > loc){
+      return &curr->val[curr->first+(loc-currLoc)];
+    }
+    currLoc += curr->last - curr->first;
+    curr = curr->next;
+  }
+
+  return NULL;
+}
